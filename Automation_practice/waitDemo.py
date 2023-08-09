a8ba50cbef22874d107619ad55b3a3df6c386f7a
Sp0643 <105235131+Sp0643@users.noreply.github.com>
2023-08-09 21:35:25 +0530

    Update waitDemo.py

--- Automation_practice/waitDemo.py
+++ Automation_practice/waitDemo.py
@@ -29,5 +29,37 @@ print(driver.find_element_by_css_selector("span.promoInfo").text
 
 
 
+Certainly! Here's an example code snippet using the `shareplum` library to connect to SharePoint and perform basic actions like listing files in a document library:
 
+```python
+from shareplum import Site
+from shareplum import Office365
+
+# SharePoint site and authentication details
+site_url = "https://your-sharepoint-site-url/sites/your-site"
+username = "your-username"
+password = "your-password"
+
+# Connect to SharePoint site
+authcookie = Office365(site_url, username=username, password=password).GetCookies()
+site = Site(site_url, authcookie=authcookie)
+
+# Specify document library name
+library_name = "Shared Documents"
+
+# Get the document library
+doc_library = site.List(library_name)
+
+# List all files in the document library
+files = doc_library.GetListItems(viewname="All Documents")
+for file in files:
+    print(file["FileLeafRef"])
+
+# Close the connection
+site._session.close()
+```
+
+Make sure to replace `https://your-sharepoint-site-url/sites/your-site`, `your-username`, and `your-password` with your actual SharePoint site URL, username, and password. Additionally, you may need to install the `shareplum` library using `pip install shareplum` before running the code.
+
+This example demonstrates how to connect to a SharePoint site, authenticate, and list files in a document library. You can further extend the code to perform other actions like uploading files, creating folders, updating items, etc., using the `shareplum` library.
 
